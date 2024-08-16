0005700d3261aeeb6e591d02223ab418ad7181f8
Shaan <shanali.ozhorik@Gmail.com>
2024-08-16 14:07:16 +0500

    committed

--- public/js/gameLogic.js
+++ public/js/gameLogic.js
@@ -6,5 +6,11 @@ const chess = new Chess();
 const boardElement = document.querySelector(".chessboard");
 
 let draggedPiece = null;
-let sourceSquare = null; 
+let sourceSquare = null;
 let playerRole = null;
+
+const renderBoard = () => {};
+
+const handleMove = () => {};
+
+const getPieceUnicode = () => {};

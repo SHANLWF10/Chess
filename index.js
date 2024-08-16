3fe3fbeab6f36636efb5f0ca4eaa5b845191aede
Shaan <shanali.ozhorik@Gmail.com>
2024-08-16 13:08:14 +0500

    first commit

--- index.js
+++ index.js
@@ -3,8 +3,24 @@ const socket = require("socket.io");
 const http = require("http");
 const { Chess } = require("chess.js");
 const exp = require("constants");
+const path = require("path");
 
 const app = express();
 
 const server = http.createServer(app);
 const io = socket(server);
+const chess = new Chess();
+let players = {};
+let currentPlayer = "W";
+
+app.set("view engine", "ejs");
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.render("index", { title: "Chess Game" });
+});
+
+server.listen(3000, () => {
+  console.log("running on port 3000");
+});

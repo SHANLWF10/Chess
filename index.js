445d9e38f87312b694b05e2d890e2bc5f5f797ca
Shaan <shanali.ozhorik@Gmail.com>
2024-08-16 17:17:10 +0500

    committed

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.set("view engine", "ejs");
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res) => {
+app.get("https://chess-one-zeta.vercel.app/", (req, res) => {
   res.render("index", { title: "Chess Game" });
 });
 

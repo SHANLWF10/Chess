b935a6117863174791751d30cb3507ef18f40ca7
Shaan <shanali.ozhorik@Gmail.com>
2024-08-16 14:02:11 +0500

    committed

--- public/js/gameLogic.js
+++ public/js/gameLogic.js
@@ -3,3 +3,8 @@ const { Chess } = require("chess.js");
 const socket = io();
 const chess = new Chess();
 
+const boardElement = document.querySelector(".chessboard");
+
+let draggedPiece = null;
+let sourceSquare = null; 
+let playerRole = null;

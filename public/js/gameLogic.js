b738f41045488e524b2f323d9be8a920ee00314c
Shaan <shanali.ozhorik@Gmail.com>
2024-08-16 14:32:38 +0500

    committed

--- public/js/gameLogic.js
+++ public/js/gameLogic.js
@@ -9,8 +9,42 @@ let draggedPiece = null;
 let sourceSquare = null;
 let playerRole = null;
 
-const renderBoard = () => {};
+const renderBoard = () => {
+  const board = chess.board();
+  boardElement.innerHTML = "";
+  board.forEach((row, rowindex) => {
+    row.forEach((square, squareindex) => {
+      const squareElem = document.createElement("div");
+      squareElem.classList.add(
+        "square",
+        (rowindex + squareindex) % 2 === 0 ? "light" : "dark"
+      );
+
+      squareElem.dataset.row = rowindex;
+      squareElem.dataset.col = squareindex;
+
+      if (square) {
+        const pieceElem = document.createElement("div");
+        pieceElem.classList.add(
+          "piece",
+          square.color === "w" ? "white" : "black"
+        );
+        pieceElem.innerText = "";
+        pieceElem.draggable = playerRole === square.color;
+        pieceElem.addEventListener("dragstart", () => {
+          if (pieceElem.draggable) {
+            draggedPiece = pieceElem;
+            sourceSquare = { row: rowindex, col: squareindex };
+          }
+        });
+      }
+    });
+  });
+};
+
 
 const handleMove = () => {};
 
 const getPieceUnicode = () => {};
+
+renderBoard();
